cba5a066f495abbe59a73b3685da9fcd6b60edc1
AJ Alfonso <24573242+ajalfonso@users.noreply.github.com>
2023-12-14 01:25:21 -0500

    ++activeelement condition to enter listener

--- js/main.js
+++ js/main.js
@@ -14,7 +14,10 @@ function addItem() {
 }
 
 document.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
+  if (
+    e.key === "Enter" &&
+    document.activeElement === document.getElementById("todoTxt")
+  ) {
     addItem();
   }
 });
